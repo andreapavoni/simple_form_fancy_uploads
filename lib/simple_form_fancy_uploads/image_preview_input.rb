4a8c45231b21e88530ff64086659c8985b70223a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2012-03-14 16:35:33 -0400

    Add :use_default_url options to the ImagePreviewInput class, if set to true and the image field is nil it will fall back to the   default_url method of carrierwave.

--- lib/simple_form_fancy_uploads/image_preview_input.rb
+++ lib/simple_form_fancy_uploads/image_preview_input.rb
@@ -2,8 +2,10 @@ module SimpleFormFancyUploads
   class ImagePreviewInput < SimpleForm::Inputs::FileInput
     def input
       version = input_html_options.delete(:preview_version)
+      use_default_url = options.delete(:use_default_url) || false
+
       out = ''
-      if object.send("#{attribute_name}?")
+      if object.send("#{attribute_name}?") || use_default_url
         out << template.image_tag(object.send(attribute_name).tap {|o| break o.send(version) if version}.send('url'))
       end
       (out << super).html_safe
